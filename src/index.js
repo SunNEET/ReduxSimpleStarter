b7a3ad7047f79f5dcb4ba9959d01b7d91351391c
Sunny <sunnyinvadeher@gmail.com>
2018-09-04 18:43:35 +0800

    controlled component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = "AIzaSyAe0vpnq2Tj8INzfWwCwBPpgPbNgyDoQlA";
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
+
